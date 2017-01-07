622073022cbd9b312ed5d919a702de60d456f603
yogiman <xuyuji9000@163.com>
2017-01-07 12:30:33 +0800

    webpack babel react && file structure

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/app.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "app.bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+};
